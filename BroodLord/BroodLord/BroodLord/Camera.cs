a9a36784e38d9f17a0234841eaef1115335e958e
zacbil <abc.zacbill@gmail.com>
2013-12-08 20:07:41 +1300

    derp commit

---
+++ BroodLord/BroodLord/BroodLord/Camera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Objects;
+
+namespace Objects
+{
+    class Camera
+    {
+        private Vector2 position;
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public void update(Vector2 toonPosition)
+        {
+            position = toonPosition;
+        }
+
+        public Matrix getTransformation(GraphicsDevice gd)
+        {
+            return Matrix.CreateTranslation(new Vector3(-position.X, -position.Y, 0)) *
+                            Matrix.CreateTranslation(new Vector3(gd.Viewport.Width * 0.5f, gd.Viewport.Height * 0.5f, 0));
+        }
+    }
+}
