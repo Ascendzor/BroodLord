ba81041a3cde2945579d629b668bb64b53de0309
unknown <bob.craker@hotmail.com>
2014-01-23 19:56:21 +1300

    MapLoader structured out

--- BroodLord/Server/MapLoader.cs
+++ BroodLord/Server/MapLoader.cs
@@ -42,48 +42,71 @@ namespace Server
                     }
                     if (pixel.R == 0 && pixel.G == 0 && pixel.B == 255)
                     {
-                        new Cat(Guid.NewGuid(), new Vector2(x * Data.TileSize, y * Data.TileSize));
+                        
                     }
                 }
             }
 
-            //Bitmap terrainMap = new Bitmap(terrainPath);
-            //LoadTerrain(terrainMap);
-        }
-
-        public static void LoadTerrain(Bitmap mapImage)
-        {
-            for (int y = 0; y < mapImage.Height; y++)
+            Bitmap terrainMap = new Bitmap(terrainPath);
+            Bitmap mobsMap = new Bitmap(mobsPath);
+            Bitmap doodadsMap = new Bitmap(doodadsPath);
+            Bitmap lootsMap = new Bitmap(lootsPath);
+            System.Drawing.Point smallestMap = GetSmallestMap(new List<Bitmap>() { terrainMap, mobsMap, doodadsMap, lootsMap });
+            for (int x = 0; x < smallestMap.X; x++)
             {
-                for (int x = 0; x < mapImage.Width; x++)
+                for (int y = 0; y < smallestMap.Y; y++)
                 {
-                    System.Drawing.Color pixel = mapImage.GetPixel(x, y);
-                    if (pixel.R == 255 && pixel.G == 0 && pixel.B == 0)
-                    {
-                        new Tree(Guid.NewGuid(), new Vector2(x * Data.TileSize, y * Data.TileSize), "tree");
-                    }
-                    if (pixel.R == 0 && pixel.G == 255 && pixel.B == 0)
-                    {
-                        new RockLoot(Guid.NewGuid(), new Vector2(x * Data.TileSize, y * Data.TileSize));
-                    }
-                    if (pixel.R == 0 && pixel.G == 0 && pixel.B == 255)
-                    {
-                        new Cat(Guid.NewGuid(), new Vector2(x * Data.TileSize, y * Data.TileSize));
-                    }
+                    LoadTerrain(x, y, terrainMap.GetPixel(x, y));
                 }
             }
         }
 
-        public static void LoadMobs(string path)
+        //get the smallest width of every submitted map
+        private static System.Drawing.Point GetSmallestMap(List<Bitmap> maps)
         {
+            System.Drawing.Point smallestMap = new System.Drawing.Point(maps[0].Width, maps[0].Height);
+            foreach (Bitmap map in maps)
+            {
+                if (map.Width < smallestMap.X)
+                {
+                    smallestMap.X = map.Width;
+                }
+                if (map.Height < smallestMap.Y)
+                {
+                    smallestMap.Y = map.Height;
+                }
+            }
+
+            return smallestMap;
         }
 
-        public static void LoadDoodads(string path)
+        public static void LoadTerrain(int x, int y, System.Drawing.Color color)
         {
+            //load terrain here
         }
 
-        public static void LoadLoots(string path)
+        public static void LoadMobs(int x, int y, System.Drawing.Color color)
         {
+            if (color.B == 255)
+            {
+                new Cat(Guid.NewGuid(), new Vector2(x * Data.TileSize, y * Data.TileSize));
+            }
+        }
+
+        public static void LoadDoodads(int x, int y, System.Drawing.Color color)
+        {
+            if (color.R == 255)
+            {
+                new Tree(Guid.NewGuid(), new Vector2(x * Data.TileSize, y * Data.TileSize), "tree");
+            }
+        }
+
+        public static void LoadLoots(int x, int y, System.Drawing.Color color)
+        {
+            if (color.G == 255)
+            {
+                new RockLoot(Guid.NewGuid(), new Vector2(x * Data.TileSize, y * Data.TileSize));
+            }
         }
     }
 }
